caa92bdc48da037831ed9fa64f919da974d0f9de
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-02-05 21:39:55 +0300

    aster

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+function App(){
+    const [myImage,setMyImage]=useState([])
+    const [isLoaded,setIsLoaded]=useState(false)
+    useEffect(()=>
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res)=>res.json())
+    .then((data)=>{
+        setMyImage(data)
+        setIsLoaded(true)
+    }
+        )
+    ,[])
+    if(!isLoaded){
+        return (
+            <p>Loading...</p>
+        )
+    }
+    /*else{
+        {myImage.map((image)=>(
+            <img src={image.src} alt="A Randon dog"/>
+        ))}
+    }*/
+    return(
+        <div>{myImage.map((image)=>(
+            <img src={image} alt="A Randon dog"/>
+        ))}</div>      
+    )
+}
+export default App

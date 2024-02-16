cc8f0f840d09942a611cc196f7986f660666d87f
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-02-16 09:21:57 +0300

    Made chnges to the lab and it worked

--- src/components/App.js
+++ src/components/App.js
@@ -3,30 +3,41 @@ import React, { useEffect, useState } from "react";
 function App(){
     const [myImage,setMyImage]=useState([])
     const [isLoaded,setIsLoaded]=useState(false)
-    useEffect(()=>
-   fetchData(),[]
-        
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res)=>res.json())
+        .then((data)=>{
+            setMyImage(data.message)
+            setIsLoaded(true)
+            console.log(data)
+        })
+        //fetchData()
+    },[]
+    
     )
- async   function fetchData(){
+/* async   function fetchData(){
 const response=await fetch("https://dog.ceo/api/breeds/image/random")
 const data=await response.json()
 setMyImage(data)
 setIsLoaded(true)
-console.log(data)
-console.log(myImage)
+//console.log(data)
+//console.log(myImage)
  }
+// console.log(myImage)*/
+ 
     if(!isLoaded){
         return (
             <p>Loading...</p>
         )
+    } 
+        return(
+            
+                <img src={myImage} alt="A Random Dog"/>
+            
+        )
     }
-    else{
-        alert('ERROR loading data')
-    }
-    return(
-        <div>{myImage.map((image)=>(
-            <img src={image} alt="A Randon dog"/>
-        ))}</div>      
-    )
-}
+     
+    
+    
+
 export default App
